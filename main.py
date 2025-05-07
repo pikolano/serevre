3218736dfc7fe8d2db004bca25d2cf7121dff0e9
pikolano <141550872+pikolano@users.noreply.github.com>
2025-05-07 10:52:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# server.py
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Хранение количества реакций
+# Хранение реакций
 reactions = {
     "Лайк!": 0,
     "Страшно...": 0,
@@ -15,20 +14,25 @@ reactions = {
 
 @app.route('/')
 def index():
-    return 'Сервер реакций работает!'
+    return "Сервер работает"
 
+# Событие для получения реакций
+@socketio.on('get_reactions')
+def handle_get_reactions():
+    print("Получен запрос на реакцию")
+    emit('update_reactions', reactions, broadcast=False)
+
+# Событие для отправки реакции
 @socketio.on('send_reaction')
-def handle_reaction(data):
-    reaction = data.get("reaction")
+def handle_send_reaction(data):
+    reaction = data.get('reaction')
     if reaction in reactions:
         reactions[reaction] += 1
+        print(f"Реакция {reaction} обновлена")
         emit('update_reactions', reactions, broadcast=True)
-
-@socketio.on('get_reactions')
-def send_initial_data():
-    emit('update_reactions', reactions)
+    else:
+        print(f"Неизвестная реакция: {reaction}")
 
 if __name__ == '__main__':
-     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
-
+    socketio.run(app, host='0.0.0.0', port=5000)
 
